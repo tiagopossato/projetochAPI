4581442bf4409ef35437c65088baa176ce66cfdd
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-08-21 15:19:02 +0000

    validacao de tokem

--- api-nodejs/routes/index.js
+++ api-nodejs/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
-
 var cidades = require('../models/cidades');
+var usuarios = require('../models/usuarios');
 
-module.exports = router;
+var router = express.Router();
+var verifier = require('google-id-token-verifier');
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,7 +17,29 @@ router.post('/api/v1/cidades', validaToken, cidades.post);
 router.put('/api/v1/cidades/:id', validaToken, cidades.put);
 router.delete('/api/v1/cidades/:id', validaToken, cidades.delete);
 
+/* USUARIOS*/
+router.post('/api/v1/usuarios', validaToken, usuarios.post);
+
 function validaToken(req, res, next) {
-    console.log(req.headers['x-authority']);
+    try {
+        var clientId = '478369223387-o0p93v3phs1qeo0362utlj1ra5a6llrm.apps.googleusercontent.com';
+
+        var token = req.headers['x-authority'];
+
+        verifier.verify(token, clientId, function(err, tokenInfo) {
+            if (!err) {
+                // use tokenInfo in here. 
+                console.log(tokenInfo);
+            }
+            else {
+                console.log(err);
+            }
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
     next();
 }
+
+module.exports = router;
