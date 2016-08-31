c302e095b046d1dfc6c5fcc00cfe40c5ddf5b007
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-08-31 03:28:26 +0000

    validando token no backend

--- api-nodejs/routes/index.js
+++ api-nodejs/routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var cidades = require('../models/cidades');
 var usuarios = require('../models/usuarios');
+const https = require('https');
 
 var router = express.Router();
 var verifier = require('google-id-token-verifier');
+var GoogleAuth = require('google-auth-library');
+
+
+
+var auth = new GoogleAuth();
+
+var oauth2Client = new auth.OAuth2();
+
+var request = require('ajax-request');
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "x-authority, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "x-access_token, x-id_token, Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
@@ -22,24 +32,30 @@ router.post('/api/v1/usuarios', validaToken, usuarios.post);
 
 function validaToken(req, res, next) {
     try {
-        var clientId = '478369223387-o0p93v3phs1qeo0362utlj1ra5a6llrm.apps.googleusercontent.com';
+        var clientId = '281275352003-nrbluthgjnach2lom1u15pct6qj0lgn0.apps.googleusercontent.com';
 
-        var token = req.headers['x-authority'];
+        var access_token = req.headers['x-access_token'];
+        //var id_token = req.headers['x-id_token'];
+        //console.log(id_token);
 
-        verifier.verify(token, clientId, function(err, tokenInfo) {
-            if (!err) {
-                // use tokenInfo in here. 
-                console.log(tokenInfo);
-            }
-            else {
-                console.log(err);
-            }
+        https.get('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + access_token, (res) => {
+            console.log('statusCode:', res.statusCode);
+            //console.log('headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+
+        }).on('error', (e) => {
+            console.error(e);
         });
+
     }
     catch (err) {
-        console.log(err);
+        console.log("Erro geral:" + err);
     }
     next();
 }
+
 
 module.exports = router;
