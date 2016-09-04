cc488d9778331bf9e002699d601d89f8b26e2615
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-09-04 14:24:22 +0000

    alterado forma de intalação (teste)

---
+++ commit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Comentarios do commit: "
+read MSG
+
+git add api-nodejs/bin/. api-nodejs/models/. api-nodejs/routes/. api-nodejs/app.js api-nodejs/package.json commit.sh
+git commit -m "$MSG"
+git push -u origin master
