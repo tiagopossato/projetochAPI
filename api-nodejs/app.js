f24f4a967445cf7061dfd9e943ba1df6866e09aa
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-09-18 02:13:52 +0000

    cidade v 0.0.1

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -1,27 +1,8 @@
 var express = require('express');
-//var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
 
 var routes = require('./routes/api-v1');
 
 var app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({
-//  extended: false
-//}));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1', routes);
 
@@ -36,8 +17,9 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-/*
+
 if (app.get('env') === 'development') {
+  
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     // res.render('error', {
@@ -46,7 +28,7 @@ if (app.get('env') === 'development') {
     // });
   });
 }
-*/
+
 
 // production error handler
 // no stacktraces leaked to user
