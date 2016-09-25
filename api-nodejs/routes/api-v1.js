dd654c795e4f5a8694e79496008c14a03515fc87
Tiago Possato <tiago@debian>
2016-09-25 08:31:22 -0300

    recuperando dados do usuario pelo access_token

--- api-nodejs/routes/api-v1.js
+++ api-nodejs/routes/api-v1.js
@@ -36,16 +36,20 @@ function validaToken(req, res, next) {
         var access_token = req.headers['x-access_token'];
         //var id_token = req.headers['x-id_token'];
         console.log(access_token);
-
+        getUserData(access_token);
+        next();
+        return;
+        //http://stackoverflow.com/questions/16501895/how-do-i-get-user-profile-using-google-access-token
 
         https.get('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + access_token, (resposta) => {
             resposta.on('data', (d) => {
                 if(resposta.statusCode==200){
                   console.log('statusCodeGoogle:', resposta.statusCode);
-                  console.log('headersGoogle:', d);
-                  for (var key of d.keys()) {
-                  console.log(key);
-                }
+                  process.stdout.write(d);
+                //   console.log('headersGoogle:', d);
+                //   for (var key of d.keys()) {
+                //   console.log(key);
+                // }
                   next();
                 }
                 else{
@@ -55,7 +59,6 @@ function validaToken(req, res, next) {
                 });
                 }
             });
-
         }).on('error', (e) => {
              console.error("Erro try: "+e);
             return res.status(500).json({
@@ -68,6 +71,40 @@ function validaToken(req, res, next) {
     catch (err) {
         console.log("Erro geral:" + err);
     }
+}
+
+/*É PRECISO HABILITAR A API DO GOOGLE+ NO CONSOLE DO GOOGLE*/
+function getUserData(access_token){
+  var url = 'https://www.googleapis.com/plus/v1/people/me?access_token='+ access_token;
+  try {
+      https.get(url, (resposta) => {
+          resposta.on('data', (d) => {
+              if(resposta.statusCode==200){
+                console.log('statusCodeGoogle:', resposta.statusCode);
+                process.stdout.write(d);
+                // next();
+              }
+              else{
+                console.log("Falha no get dados: "+ resposta.statusCode);
+              //     return res.status(resposta.statusCode).json({
+              //     error: true,
+              //     data: "Falha na autenticação!"
+              // });
+              }
+          });
+      }).on('error', (e) => {
+           console.error("Erro try: "+e);
+          // return res.status(500).json({
+          //         success: false,
+          //         data: e.message
+          //     });
+      });
+
+  }
+  catch (err) {
+      console.log("Erro geral:" + err);
+  }
+
 }
 
 /*USUARIO*/
