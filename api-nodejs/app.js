e56b2cd21affa45d51c7d88f9fa7003d8e562235
Tiago Possato <tiago.possato@yahoo.com.br>
2016-10-14 08:23:49 -0300

    retornar usuario

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -5,9 +5,11 @@ var routes = require('./routes/api-v1');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.use('/api/v1', routes);
+app.use('/v1', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
