880e8656311124d59f812601cce7f7da33a0f687
Tiago Possato <tiago.possato@yahoo.com.br>
2016-10-14 08:34:29 -0300

    retornar usuario

--- api-nodejs/models/Endereco.js
+++ api-nodejs/models/Endereco.js
@@ -1,16 +1,22 @@
 "use strict";
 let bookshelf = require('bookshelf')(require('../models/banco'));
 bookshelf.plugin('visibility');
+let Usuario = require('../models/Usuario');
+
 // let Cidade = require('../models/Cidade');
 // let moment = require('moment-timezone');
 
 let Endereco = bookshelf.Model.extend({
 	tableName: 'ENDERECO',
 	idAttribute: 'endCodigo',
-	hidden: ['endUpdatedAt', 'endCreatedAt']
-		// cidade: function() {
-		// 	return this.hasOne(Cidade, 'cidCodigo');
-		// },
+	hidden: ['endUpdatedAt', 'endCreatedAt'],
+	usuario: function() {
+		return this.belongsTo(Usuario);
+	}
+
+	// cidade: function() {
+	// 	return this.hasOne(Cidade, 'cidCodigo');
+	// },
 });
 
 //let Enderecos = bookshelf.Collection.extend({
