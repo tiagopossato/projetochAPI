c90e17493b06d54dfea7d0cd74bca502bb5882bb
Tiago Possato <tiago.possato@yahoo.com.br>
2016-11-23 10:02:53 -0200

    tentando...

--- api-nodejs/controls/NotificacoesController.js
+++ api-nodejs/controls/NotificacoesController.js
@@ -65,18 +65,35 @@ function enviaNotificacao(idGoogle, msg) {
 
 function enviaNotificacaoGeral(msg) {
   var mensagem = {
-    to = 'br.com.projetoch.hortapp',
-      notification: {
-        title: 'Aplicativo HortApp',
-        body: msg
-      }
+    notification: {
+      title: 'Aplicativo HortApp',
+      body: msg
+    }
   };
-  fcm.send(mensagem)
-    .then(function(response) {
-      console.log("Mensagem enviada");
+  //console.log(mensagem);
+  usuModel.Usuario
+    .fetch()
+    .then(function(user) {
+        //console.log(user.toJSON());
+        let i = 0;
+        for (; i < user.length; i++) {}
+        usuario = user[i].toJSON();
+        var tokenFcm = usuario['usuTokenFcm'];
+        if (tokenFcm) {
+          mensagem['to'] = tokenFcm;
+          //console.log(mensagem);
+          fcm.send(mensagem)
+            .then(function(response) {
+              console.log("Mensagem enviada");
+            })
+            .catch(function(err) {
+              console.log("Something has gone wrong!");
+              console.error(err);
+            });
+        }
+      }
     })
-    .catch(function(err) {
-      console.log("Something has gone wrong!");
-      console.error(err);
-    });
+.catch(function(err) {
+  console.log('enviaNotificacao->Usuario.where:' + err);
+});
 }
